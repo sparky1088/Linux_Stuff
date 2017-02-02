c7597e870e2b4ff591513f32a864712cb6ea3e85
sparky1088 <sparky1088@gmail.com>
2017-02-02 11:10:50 -0700

    Made it so that folders will work so I can organize images.

--- backgroundchanger.sh
+++ backgroundchanger.sh
@@ -4,5 +4,5 @@ export DISPLAY=:0
 
 #DIR="/home/mwaldorf/Pictures/wallpapers/WallpapersDualScreen/"
 DIR="/home/mwaldorf/Pictures/wallpapers/"
-PIC=$(ls $DIR/* | shuf -n1)
+PIC=$(find $DIR/ -type f | shuf -n1)
 gsettings set org.gnome.desktop.background picture-uri "file://$PIC"
