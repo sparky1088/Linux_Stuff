9830b79d6f94e1eff37f1a4557b632a2fc2d7339
sparky1088 <sparky1088@gmail.com>
2019-03-18 13:48:46 -0700

    ubooquity.sh
    Adding file to send changes to dev

---
+++ scripts/ubooquity.sh
@@ -0,0 +1,54 @@
+#!/bin/bash 
+
+UBOOQUITY_PORT=2202
+ADMIN_PORT=2233
+# Uncomment the following line to allocate more memory to Ubooquity
+# MEM_OPT=-Xmx512m
+
+count=`pgrep -f Ubooquity.jar`
+
+case "$1" in
+        start)
+			if [ -z "$count" ];
+				then
+				    nohup java -Djava.awt.headless=true $MEM_OPT -jar Ubooquity.jar --libraryport $UBOOQUITY_PORT --headless --adminport $ADMIN_PORT --remoteadmin </dev/null &>/srv/ubooquity/logs/ubooquity.log &
+					echo Ubooquity has been started;
+				else
+					echo Ubooquity is already running;
+			fi            
+            ;;
+         
+        stop)
+			if [ -z "$count" ];
+				then
+				    echo Ubooquity is NOT running, no need to stop it;
+				else 
+					echo stopping Ubooquity...
+					pkill -f "Ubooquity.jar" </dev/null &>/dev/null &
+					echo Waiting 10 seconds before checking the server stopped...;
+					sleep 10
+
+					check=`pgrep -f Ubooquity.jar`
+					if [ -z "$check" ];
+						then
+							echo Ubooquity has been stopped;
+						else
+							echo Failed to stop Ubooquity: you can either wait a few more seconds or kill it manually;
+					fi
+			fi
+            ;;
+         
+        status)
+			if [ -z "$count" ];
+				then 
+					echo Ubooquity is NOT running;
+				else
+				    echo Ubooquity is RUNNING;
+			fi
+            ;;
+			
+        *)
+            echo $"Usage: $0 {start|stop|status}"
+            exit 1
+ 
+esac
